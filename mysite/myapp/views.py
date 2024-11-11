ffa5e6ce86f25d6a02db9559d520cc0e359a4796
0k1nd <akindtyrbo@gmail.com>
2024-11-11 21:51:37 +0300

    try create group permission

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.views.generic import CreateView, UpdateView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
@@ -8,18 +11,23 @@ from rest_framework.viewsets import ModelViewSet
 # from mysite.myapp.forms import PostForm
 from myapp.serializers import TaskSerializer, CommentSerializer
 from myapp.models import Task, Comment
+from myapp.permitions import IsEditor
 
 
-
-class TaskViewSet(ModelViewSet):
+class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     queryset = Task.objects.all()
-    # permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['status']
     search_fields = ['created_at', 'assignee.id']
-    #ordering_fields = ['price', 'author_name']
+    # ordering_fields = ['price', 'author_name']
+    permission_classes = [IsEditor]
 
+    def get_queryset(self):
+        user = self.request.user
+        if not self.request.user.groups.filter(name='editor').exists():
+            queryset = queryset.filter(assignee=self.request.user)
+            return queryset
 
 class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
