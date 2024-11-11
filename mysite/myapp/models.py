42ba53bf9fcc7bc397aac1a04c5bc5e9971b3413
charmantemort <dawnwestmorland45@gmail.com>
2024-11-11 15:34:41 +0400

    jwt-token

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.db.models import CharField
+from rest_framework.authtoken.admin import User
+from rest_framework.viewsets import ModelViewSet
 
 
-# Create your models here.
+class Project(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
